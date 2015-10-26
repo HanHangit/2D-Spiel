2e6a21cecf2b27789a2cf4d5e7412881db5fcb62
schmelix <FelixSchmekel@gmx.de>
2015-10-26 19:26:46 +0100

    dfgghghfh

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -13,13 +13,17 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            RenderWindow window = new RenderWindow(new VideoMode(800, 600), "Test01");
+            RenderWindow window = new RenderWindow(new VideoMode(800, 600), "The Bug Bang Theory");
             window.Closed += (object sender, EventArgs e) => { (sender as Window).Close(); };
             while (window.IsOpen())
             {
-                window.Clear();
+                window.Clear(new Color(0,128,0,0));
                 window.Display();
                 window.DispatchEvents();
+                RectangleShape rechteck = new RectangleShape();
+                rechteck.Position = new Vector2f(12, 12);
+                rechteck.Size = new Vector2f(320, 320);
+                rechteck.FillColor = new Color(0, 0, 255,0);              
             }
         }
     }
