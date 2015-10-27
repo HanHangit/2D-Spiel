ba4d72bdce09efe7a972235162a03230679168fe
schmelix <FelixSchmekel@gmx.de>
2015-10-27 11:33:51 +0100

    zrdtd

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -15,16 +15,19 @@ namespace Test
         {
             RenderWindow window = new RenderWindow(new VideoMode(800, 600), "The Bug Bang Theory");
             window.Closed += (object sender, EventArgs e) => { (sender as Window).Close(); };
+            RectangleShape rechteck = new RectangleShape(new Vector2f(320, 320));
+            rechteck.Position = new Vector2f(12, 12);
+            rechteck.FillColor = new Color(0, 0, 255);
             while (window.IsOpen())
             {
                 window.Clear(new Color(0,128,0,0));
+                
+                
+                window.Draw(rechteck);
+               
                 window.Display();
                 window.DispatchEvents();
-                RectangleShape rechteck = new RectangleShape(new Vector2f(320, 320));
-                window.Draw(rechteck);
-                rechteck.Position = new Vector2f(12, 12);
-                rechteck.FillColor = new Color(0, 0, 255);
-                              
+
             }
         }
     }
