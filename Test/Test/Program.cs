03db13683516dc2e2bc721e8449facebfd7d86b0
HanHangit <clonkcool@yahoo.de>
2015-10-27 13:13:38 +0100

    Noch mehr Änderungen

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -16,11 +16,11 @@ namespace Test
             RenderWindow window = new RenderWindow(new VideoMode(800, 600), "The Bug Bang Theory");
             window.Closed += (object sender, EventArgs e) => { (sender as Window).Close(); };
                 RectangleShape rechteck = new RectangleShape(new Vector2f(320, 320));
-                rechteck.Position = new Vector2f(12, 12);
+                rechteck.Position = new Vector2f(50, 12);
                 rechteck.FillColor = new Color(0, 0, 255);
 
             CircleShape kreis = new CircleShape(50);
-            kreis.Position = new Vector2f(400, 400);
+            kreis.Position = new Vector2f(200, 200);
             kreis.FillColor = new Color(255, 0, 0);
 
                 
