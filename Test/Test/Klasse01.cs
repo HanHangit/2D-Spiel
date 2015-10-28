4a64564e0d389237548dc4e18b9f1aa22d2467a7
HanHangit <clonkcool@yahoo.de>
2015-10-28 17:13:38 +0100

    Klassen-Test

---
+++ Test/Test/Klasse01.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML;
+using SFML.Window;
+using SFML.Graphics;
+
+public class Class1
+{
+	public Class1()
+	{
+        int i = 10;
+	}
+}
