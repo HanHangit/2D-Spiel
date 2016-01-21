7ab3c843ba0f6db16ebe5736f21112f56d462ca6
HanHangit <clonkcool@yahoo.de>
2016-01-21 11:55:30 +0100

    Hud fix

---
+++ Test/Test/Hud.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML;
+using SFML.Window;
+using SFML.Graphics;
+
+namespace Test
+{
+    class Hud
+    {
+        private View view;
+        private String time;
+        private String leben;
+        public Hud()
+        {
+
+        }
+        public void setView(View view)
+        {
+            this.view = view;
+        }
+        public void Update(GameTime gTime)
+        {
+
+        }
+        public void Draw(Window win)
+        {
+
+        }
+    }
+}
